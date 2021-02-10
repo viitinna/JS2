3f135d7d87bec260a586503b09c029440cf4f2da
viitinna <71917435+viitinna@users.noreply.github.com>
2021-02-10 23:07:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+
+//for (let i = 0; i < 10; i++) {
+	//document.write("<div>Test</div>");
+	//document.write(`<div>Test : ${i} </div>`)
+//}
+
+
+//let i = 0;
+//while(i < 20) {
+	//document.write("<h1>good</h1>");
+	//document.write(`<h1>good : ${i} </h1>`)
+	//i++;
+//}
+
+
+//const nam = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+//for (let i = 0; i < nam.length; i++) {
+	//console.log(nam[i]);
+//}
+
+
+//const Name = ["Inna", "Alla", "Oleg", "Ira", "Diana", "Sveta", "Andriy", "Artem", "Bella", "Joy"];
+//for (let i = 0; i < Name.length; i++) {
+	//console.log(Name[i]);
+//}
+
+
+//const mix = ["Inna", "Alla", true, "Ira", "Diana", 11, "Andriy", -89, "Bella", "Joy"];
+//for (let i = 0; i < mix.length; i++) {
+	//console.log(mix[i]);
+//}
+
+
+//const mix = ["Inna", "Alla", true, "Ira", "Diana", 11, "Andriy", -89, "Bella", "Joy"];
+//for (let i = 0; i < mix.length; i++) {
+	//if(typeof mix[i] === "boolean")
+	//console.log(mix[i]);
+//}
+
+
+//const mix = ["Inna", "Alla", true, "Ira", "Diana", 11, "Andriy", -89, "Bella", "Joy"];
+//for (let i = 0; i < mix.length; i++) {
+	//if(typeof mix[i] === "number")
+	//console.log(mix[i]);
+//}
+
+
+//const mix = ["Inna", "Alla", true, "Ira", "Diana", 11, "Andriy", -89, "Bella", "Joy"];
+//for (let i = 0; i < mix.length; i++) {
+	//if(typeof mix[i] === "string")
+	//console.log(mix[i]);
+//}
+
+
+//const zero = [];
+//zero[1] = "Inna";
+//zero[2] = true;
+//zero[3] = "Alla";
+//zero[4] = 22;
+//zero[5] = -99;
+//zero[6] = "Oleg";
+//zero[7] = false;
+//zero[8] = "Diana";
+//zero[9] = 100;
+
+//for (let i = 0; i < zero.length; i++) {
+	//console.log(zero[i]);
+//}
+
+
+//const zero = [];
+//zero[1] = "Inna";
+//zero[2] = true;
+//zero[3] = "Alla";
+//zero[4] = 22;
+//zero[5] = -99;
+//zero[6] = "Oleg";
+//zero[7] = false;
+//zero[8] = "Diana";
+//zero[9] = 100;
+
+//for (let i = 0; i < 10; i++) {
+	//console.log(zero[i]);
+	//document.write(zero[i]);
+//}
+
+
+//const zero = [];
+//zero[1] = "Inna";
+//zero[2] = true;
+//zero[3] = "Alla";
+//zero[4] = 22;
+//zero[5] = -99;
+//zero[6] = "Oleg";
+//zero[7] = false;
+//zero[8] = "Diana";
+//zero[9] = 100;
+
+//for (let i = 0; i < 100; i++) {
+	//console.log(zero[i]);
+	//document.write(zero[i]);
+//}
+
+
+let step = 0
+for (let i = 0; i < 100; i++) {
+	if(i%2 === 1){
+	console.log("step : ",i);
+	document.write("step : ",i);
+}
+
+}
+
+
